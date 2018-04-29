8738ed7d22131b94dc11063021e31d5e3d5527df
ilya_shusterman <shusterilyaman@gmail.com>
2018-04-29 19:12:45 +0300

    added test schematics

---
+++ word_count_machine/server.py
@@ -0,0 +1,23 @@
+import os
+import tornado.ioloop
+import tornado.web
+from tornado.options import parse_command_line
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+if __name__ == '__main__':
+    parse_command_line()
+    app = make_app()
+    port = int(os.environ.get('PORT', 5000))
+    app.listen(port)
+    tornado.ioloop.IOLoop.current().start()
---
+++ word_count_machine/test/TestServer.py
@@ -0,0 +1,8 @@
+from tornado.testing import AsyncHTTPTestCase
+
+from word_count_machine import server
+
+
+class TestServer(AsyncHTTPTestCase):
+    def get_app(self):
+        return server.make_app()
